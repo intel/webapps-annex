954c3ca6e73a8021fd2346f8ac5cfad09c6e128c
Max Waterman <max.waterman@intel.com>
2013-10-18 14:54:03 +0100

    Using grunt templates to get the app version number/etc from a single place, package.json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     packageInfo: grunt.file.readJSON('package.json'),
-    chromeInfo: grunt.file.readJSON('data/manifest.json'),
+    chromeInfo: grunt.file.readJSON('data/chrome-crx/manifest.json'),
 
     clean: ['build'],
 
@@ -20,9 +20,9 @@ module.exports = function (grunt) {
       // (default: '/tmp')
       tizenAppScriptDir: '/home/developer/',
 
-      // path to the config.xml file for the Tizen wgt file
+      // path to the config.xml file for the Tizen wgt file - post templating
       // (default: 'config.xml')
-      configFile: 'data/config.xml',
+      configFile: 'build/wgt/config.xml',
 
       // path to the sdb command (default: process.env.SDB or 'sdb')
       sdbCmd: 'sdb'
@@ -61,24 +61,53 @@ module.exports = function (grunt) {
       wgt: {
         files: [
           { expand: true, cwd: 'build/app/', src: ['**'], dest: 'build/wgt/' },
-          { expand: true, cwd: 'data/', src: ['config.xml'], dest: 'build/wgt/' },
           { expand: true, cwd: '.', src: ['icon_128.png'], dest: 'build/wgt/' }
         ]
       },
+
+      wgt_config: {
+        files: [
+          { expand: true, cwd: 'data/tizen-wgt/', src: ['config.xml'], dest: 'build/wgt/' }
+        ],
+        options:
+        {
+          processContent: function(content, srcpath)
+          {
+            return grunt.template.process(content);
+          }
+        }
+      },
+
       crx: {
         files: [
           { expand: true, cwd: 'build/app/', src: ['**'], dest: 'build/crx/' },
-          { expand: true, cwd: '.', src: ['manifest.json'], dest: 'build/crx/' },
           { expand: true, cwd: '.', src: ['icon*.png'], dest: 'build/crx/' }
         ]
       },
+
+      crx_manifest:
+      {
+        files: [
+          { expand: true, cwd: 'data/chrome-crx/', src: ['manifest.json'], dest: 'build/crx/' }
+        ],
+
+        options:
+        {
+          processContent: function(content, srcpath)
+          {
+            return grunt.template.process(content);
+          }
+        }
+
+      },
+
       sdk: {
         files: [
           { expand: true, cwd: 'build/app/', src: ['**'], dest: 'build/sdk/' },
           { expand: true, cwd: 'app/', src: ['js/**'], dest: 'build/sdk/' },
           { expand: true, cwd: 'app/', src: ['css/**'], dest: 'build/sdk/' },
           { expand: true, cwd: 'app/', src: ['*.html'], dest: 'build/sdk/' },
-          { expand: true, cwd: 'data/', src: ['config.xml'], dest: 'build/sdk/' },
+          { expand: true, cwd: 'data/tizen-wgt/', src: ['config.xml'], dest: 'build/sdk/' },
           { expand: true, cwd: '.', src: ['icon*.png'], dest: 'build/sdk/' }
         ]
       }
@@ -188,14 +217,13 @@ module.exports = function (grunt) {
     'copy:common'
   ]);
 
-  grunt.registerTask('crx', ['dist', 'copy:crx']);
-  grunt.registerTask('wgt', ['dist', 'copy:wgt', 'package:wgt']);
-
+  grunt.registerTask('crx', ['dist', 'copy:crx', 'copy:crx_manifest']);
+  grunt.registerTask('wgt', ['dist', 'copy:wgt', 'copy:wgt_config', 'package:wgt']);
   grunt.registerTask('sdk', [
-    'clean',
     'imagemin:dist',
     'copy:common',
     'copy:sdk',
+    'copy:wgt_config',
     'package:sdk'
   ]);
 
@@ -204,6 +232,7 @@ module.exports = function (grunt) {
     'uglify:perf',
     'inline',
     'copy:wgt',
+    'copy:wgt_config',
     'package:wgt'
   ]);
 
